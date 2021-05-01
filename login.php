807f61cb6f9e0a88b90b46fdf6123f5adfa20c01
Jacob <jacob.e.hobday99@gmail.com>
2021-05-01 17:14:02 +0100

    Restructure & Linking Pages HUB-5
    Site is now structured with an assets folder with all pages linked.
    Cannot reproduce the image bug as of yet.

--- login.php
+++ login.php
@@ -1,2 +1,16 @@
-<?php echo htmlspecialchars($_POST['username', 'password']); ?>
+<?php echo htmlspecialchars($_POST['username', 'password']);
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "userlist";
+$tablename = "users";
+
+$connect = new mysqli($servername, $username, $password, $dbname);
+if ($connect->connection_aborted || $connect->connect_error){
+    die("Connection failed: " . $conn->connect_error)
+}
+
+
+?>
 
